e7df43e30aeee401e0b299353b878fcae2157063
Ronan Scotet <102806074+domov44@users.noreply.github.com>
2024-05-30 15:22:06 +0200

    form controller user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,24 +4,41 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\UserRepository;
 use App\Entity\User;
+use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\MakerBundle\Doctrine\EntityRegenerator;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class UserController extends AbstractController
 {
     #[Route('/user/create', name: 'create_user')]
-    public function createUser(EntityManagerInterface $entityManager): Response
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $user = new User();
-        $user->setName('John');
-        $user->setPhone(045776);
+        $form = $this->createFormBuilder($user)
+            ->add('name', TextType::class, ['label' => 'Username'])
+            ->add('phone', IntegerType::class, ['label' => 'Phone Number'])
+            ->add('save', SubmitType::class, ['label' => 'Create User'])
+            ->getForm();
 
-        $entityManager->persist($user);
-        $entityManager->flush();
+        $form->handleRequest($request);
 
-        return new Response('Saved new user with id ' . $user->getId());
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($user);
+            $entityManager->flush();
+            $this->addFlash('success', 'User created successfully!');
+            return $this->redirectToRoute('user_index');
+        }
+
+        return $this->render('user/create.html.twig', [
+            'form' => $form
+        ]);
     }
 
     #[Route('/user/{id}', name: 'user_show', methods: ['GET'])]
